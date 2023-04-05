857c7403b39ee396b24ca409991a71dac6aa39b0
tnov <tnov@bach.cz>
2023-04-05 15:05:30 +0200

    wgs/om coverage plot

--- src/main/java/cz/vsb/genetics/coverage/main/CoveragePlot.java
+++ src/main/java/cz/vsb/genetics/coverage/main/CoveragePlot.java
@@ -4,31 +4,93 @@ import cz.vsb.genetics.coverage.CoverageInfo;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartUtils;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.data.category.DefaultCategoryDataset;
-import org.jfree.data.flow.DefaultFlowDataset;
-import org.jfree.data.general.DefaultValueDataset;
+import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 
 import java.io.File;
+import java.util.Random;
 
 public class CoveragePlot {
-    public void plotCoverage(CoverageInfo coverageInfo, String title, String labelX, String labelY, String outputFile) throws Exception {
-        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+    private int width = 1600;
+    private int height = 1200;
 
-        int[] coverage = coverageInfo.getCoverages();
-
-        for (int i = 0; i < coverage.length; i++) {
-            dataset.addValue((double) coverage[i], "", Integer.valueOf(i));
-        }
-
-        JFreeChart coverageChart = ChartFactory.createLineChart(title, labelX, labelY, dataset,
+    public void plotCoverage(String title, String xLabel, String yLabel, String outputFile, CoverageInfo... coverageInfos) throws Exception {
+        JFreeChart coverageChart = ChartFactory.createXYStepChart(title, xLabel, yLabel, createXYDataset(coverageInfos),
                 PlotOrientation.VERTICAL, true, true, false);
 
-        int width = 1600;    /* Width of the image */
-        int height = 1200;   /* Height of the image */
+        NumberAxis xAxis = new NumberAxis(xLabel);
+        xAxis.setRange(coverageInfos[0].getPositionStart(), coverageInfos[0].getPositionEnd());
+        coverageChart.getXYPlot().setDomainAxis(xAxis);
 
         File lineChart = new File( outputFile);
         ChartUtils.saveChartAsJPEG(lineChart , coverageChart, width ,height);
+    }
+
+    public void plotCoverage(String title, String xLabel, String yLabel, String outputFile, int sampleSize, CoverageInfo... coverageInfos) throws Exception {
+        JFreeChart coverageChart = ChartFactory.createXYStepChart(title, xLabel, yLabel, createXYDataset(coverageInfos, sampleSize),
+                PlotOrientation.VERTICAL, true, true, false);
+
+        NumberAxis xAxis = new NumberAxis(xLabel);
+        xAxis.setRange(coverageInfos[0].getPositionStart(), coverageInfos[0].getPositionEnd());
+        coverageChart.getXYPlot().setDomainAxis(xAxis);
+
+        File lineChart = new File( outputFile);
+        ChartUtils.saveChartAsJPEG(lineChart , coverageChart, width ,height);
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    XYDataset createXYDataset(CoverageInfo[] coverageInfos) {
+        XYSeriesCollection dataset = new XYSeriesCollection();
+
+        for (CoverageInfo coverageInfo : coverageInfos) {
+            XYSeries xySeries = new XYSeries(coverageInfo.getTitle());
+
+            int[] coverage = coverageInfo.getCoverages();
+
+            for (int i = 0, j = coverageInfo.getPositionStart(); i < coverage.length; i++, j++)
+                xySeries.add(j, coverage[i]);
+
+            dataset.addSeries(xySeries);
+        }
+
+        return dataset;
+    }
+
+    XYDataset createXYDataset(CoverageInfo[] coverageInfos, int sampleSize) {
+        XYSeriesCollection dataset = new XYSeriesCollection();
+
+        for (CoverageInfo coverageInfo : coverageInfos) {
+            XYSeries xySeries = new XYSeries(coverageInfo.getTitle());
+
+            int[] coverage = coverageInfo.getCoverages();
+            Random random = new Random();
+
+            sampleSize -= -1;
+
+            for (int i = 0, j = coverageInfo.getPositionStart(); i < coverage.length; i+= sampleSize, j+= sampleSize) {
+                int index = random.nextInt(sampleSize) + i;
+
+                if (index >= coverage.length) {
+                    index = coverage.length - 1;
+                    j = coverageInfo.getPositionEnd();
+                }
+
+                xySeries.add(j, coverage[index]);
+            }
+
+            dataset.addSeries(xySeries);
+        }
+
+        return dataset;
     }
 }
--- src/test/java/cz/vsb/genetics/coverage/test/TestCoveragePlot.java
+++ src/test/java/cz/vsb/genetics/coverage/test/TestCoveragePlot.java
@@ -25,6 +25,7 @@ import cz.vsb.genetics.coverage.CoverageInfo;
 import cz.vsb.genetics.coverage.main.CoveragePlot;
 import cz.vsb.genetics.ngs.coverage.BamCoverageCalculatorMT;
 import cz.vsb.genetics.ngs.coverage.BamCoverageCalculatorST;
+import cz.vsb.genetics.om.coverage.BionanoCoverageCalculator;
 import org.apache.commons.lang3.time.StopWatch;
 
 import java.text.DateFormat;
@@ -33,22 +34,39 @@ import java.util.Date;
 import java.util.TimeZone;
 
 public class TestCoveragePlot {
-    private static final String BAM_FILE = "./src/test/resources/coverage/bam/test.bam";
-    private static final String BAM_INDEX_FILE = "./src/test/resources/coverage/bam/test.bai";
+//    private static final String BAM_FILE = "./src/test/resources/coverage/bam/test.bam";
+//    private static final String BAM_INDEX_FILE = "./src/test/resources/coverage/bam/test.bai";
+    private static final String BAM_FILE = "c:\\data\\fnol\\wgs\\linkedread\\phased_possorted_bam.chr21.bam";
+    private static final String BAM_INDEX_FILE = "c:\\data\\fnol\\wgs\\linkedread\\phased_possorted_bam.chr21.bam.bai";
     private static final String BAM_COVERAGE_PLOT_ST = "./src/test/resources/coverage/bam/test.coverage.st.jpg";
     private static final String BAM_COVERAGE_PLOT_MT = "./src/test/resources/coverage/bam/test.coverage.mt.jpg";
 
+    private static final String CMAP_REF = "./src/test/resources/coverage/bionano/test_ref.cmap";
+    private static final String CMAP_QRY = "./src/test/resources/coverage/bionano/test_query.cmap";
+    private static final String XMAP = "./src/test/resources/coverage/bionano/test.xmap";
+    private static final String OM_COVERAGE_PLOT = "./src/test/resources/coverage/bam/test.coverage.om.jpg";
+
+    private static final String OM_BAM_COVERAGE_PLOT = "./src/test/resources/coverage/bam/test.coverage.om.bam.jpg";
+
     public static void main(String[] args) {
         try {
-            Chromosome chromosome = Chromosome.chr17;
-//            int start = 7675023;
-//            int end = 7675999;
+//            Chromosome chromosome = Chromosome.chr17;
+//
+//            //TP53
+//            int start = 7668421;
+//            int end = 7687490;
 
+            Chromosome chromosome = Chromosome.chr21;
             int start = 1;
-            int end = 83257441;
+            int end = 46709983;
 
-            testBamCoverageAtIntervalST(chromosome, start, end);
-            testBamCoverageAtIntervalMT(chromosome, start, end, 6);
+//            int start = 16180000;
+//            int end = 16190000;
+
+            //testBamCoverageAtIntervalST(chromosome, start, end);
+            //testBamCoverageAtIntervalMT(chromosome, start, end, 6);
+            //testOmCoverageAtInterval(chromosome, start, end);
+            testWgsOmCoverage(chromosome, start, end, 6);
         }
         catch (Exception e) {
             System.out.println(e.getMessage() + "\n");
@@ -71,7 +89,7 @@ public class TestCoveragePlot {
         printTime(stopWatch.getTime());
 
         CoveragePlot coveragePlot = new CoveragePlot();
-        coveragePlot.plotCoverage(coverageInfo, "Chromosome 17", "position", "coverage", BAM_COVERAGE_PLOT_ST);
+        coveragePlot.plotCoverage("Chromosome 21", "position", "coverage", BAM_COVERAGE_PLOT_ST, coverageInfo);
     }
 
     public static void testBamCoverageAtIntervalMT(Chromosome chromosome, int start, int end, int threads) throws Exception {
@@ -89,7 +107,50 @@ public class TestCoveragePlot {
         printTime(stopWatch.getTime());
 
         CoveragePlot coveragePlot = new CoveragePlot();
-        coveragePlot.plotCoverage(coverageInfo, "Chromosome 17", "position", "coverage", BAM_COVERAGE_PLOT_MT);
+        coveragePlot.plotCoverage("Chromosome 21", "position", "coverage", BAM_COVERAGE_PLOT_MT, coverageInfo);
+    }
+
+    public static void testOmCoverageAtInterval(Chromosome chromosome, int start, int end) throws Exception {
+        System.out.println("\nTesting OM coverage info at interval - single-threaded.");
+
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        CoverageCalculator coverageCalculator = new BionanoCoverageCalculator(CMAP_REF, CMAP_QRY, XMAP);
+        coverageCalculator.open();
+        CoverageInfo coverageInfo = coverageCalculator.getIntervalCoverage(chromosome, start, end);
+        coverageCalculator.close();
+
+        stopWatch.stop();
+        printTime(stopWatch.getTime());
+
+        CoveragePlot coveragePlot = new CoveragePlot();
+        coveragePlot.plotCoverage("Chromosome 21", "position", "coverage", OM_COVERAGE_PLOT, 10, coverageInfo);
+    }
+
+    public static void testWgsOmCoverage(Chromosome chromosome, int start, int end, int threads) throws Exception {
+        System.out.println("\nTesting WGS/OM coverage info at interval.");
+
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        CoverageCalculator coverageCalculatorOm = new BionanoCoverageCalculator(CMAP_REF, CMAP_QRY, XMAP);
+        coverageCalculatorOm.open();
+        CoverageInfo coverageInfoOm = coverageCalculatorOm.getIntervalCoverage(chromosome, start, end);
+        coverageInfoOm.setTitle("OM");
+        coverageCalculatorOm.close();
+
+        CoverageCalculator coverageCalculatorBam = new BamCoverageCalculatorMT(BAM_FILE, BAM_INDEX_FILE, threads);
+        coverageCalculatorBam.open();
+        CoverageInfo coverageInfoBam = coverageCalculatorBam.getIntervalCoverage(chromosome, start, end);
+        coverageInfoBam.setTitle("WGS");
+        coverageCalculatorBam.close();
+
+        stopWatch.stop();
+        printTime(stopWatch.getTime());
+
+        CoveragePlot coveragePlot = new CoveragePlot();
+        coveragePlot.plotCoverage("Chromosome 21", "position", "coverage", OM_BAM_COVERAGE_PLOT, 10, coverageInfoBam, coverageInfoOm);
     }
 
     private static void printTime(long mils) {
